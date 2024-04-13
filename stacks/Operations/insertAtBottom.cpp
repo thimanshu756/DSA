a861bb81e4f380d953da8cb12aed172e0bab754e
thimanshu756 <himanshu1662.be22@gmail.com>
2024-04-13 11:38:34 +0530

    insert at bottom

---
+++ stacks/Operations/insertAtBottom.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void insertAtBottom(stack<int> &s, int target){
+    // base case
+    if(s.empty()){
+        s.push(target);
+        return;
+    }
+
+    int topElement= s.top();
+    s.pop();
+
+    // reccursion call
+    insertAtBottom(s,target);
+    // backtracing
+    s.push(topElement);
+}
+
+int main(){
+
+
+    stack<int> s;
+    s.push(40);
+    s.push(30);
+    s.push(20);
+    s.push(10);
+
+    insertAtBottom(s,50);
+
+    while(!s.empty()){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+
+    return 0;
+}
