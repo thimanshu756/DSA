546b0dc701d7c8da925a00c4163fdb22f13c486d
thimanshu756 <himanshu1662.be22@gmail.com>
2024-04-14 18:16:11 +0530

    next smaller element cpp

---
+++ stacks/Operations/nextSmallerElement.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stack>
+#include <vector>
+
+using namespace std;
+
+vector<int> nextSmallerElement(vector<int> &input){
+    stack<int> st;
+    st.push(-1);
+    vector<int>ans(input.size());
+    for (int i = input.size()-1; i >0 ; i--)
+    {
+        int curr= input[i];
+        while (st.top()>=curr)
+        {
+            st.pop();
+        }
+        ans[i]=st.top();
+        st.push(curr);
+    }
+        return ans;
+}
+
+
+int main(){
+    // cout<<"reached1";
+  vector<int> input;
+  input.push_back(2);
+  input.push_back(1);
+  input.push_back(4);
+  input.push_back(3);
+
+  vector<int> ans1 = nextSmallerElement(input);
+
+  cout << "Printing ans1" << endl;
+
+  for(int i=0; i<input.size(); i++) {
+          cout << ans1[i] << " ";
+  }cout << endl;
+    return 0;
+}
